906f8f75892397ecc8c6b259961360ecbb9cb97b
Robert Franzke <r.l.franzke@gmail.com>
2020-03-21 16:23:07 +0100

    Adding Tests for Joost and Presence Stuff

---
+++ src/test/java/de/sakpaas/backend/service/OccupancyServiceTest.java
@@ -0,0 +1,16 @@
+package de.sakpaas.backend.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OccupancyServiceTest {
+
+  @Test
+  void getAverageOccupancy() {
+  }
+
+  @Test
+  void bellCurve() {
+  }
+}
