3391da10742a4c6afaa0df9536b6fc7baf511421
Robert Franzke <r.l.franzke@gmail.com>
2020-03-23 17:04:16 +0100

    Added Prometheus Exporter und Request Meter

--- src/main/java/de/sakpaas/backend/api/StatusController.java
+++ src/main/java/de/sakpaas/backend/api/StatusController.java
@@ -1,6 +1,8 @@
 package de.sakpaas.backend.api;
 
 import de.sakpaas.backend.dto.StatusDto;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,24 @@ public class StatusController {
   @Value("${app.version}")
   private String version;
 
+  private final MeterRegistry meterRegistry;
+
+  private Counter getCounter;
+
+  public StatusController(MeterRegistry meterRegistry) {
+    this.meterRegistry = meterRegistry;
+
+    getCounter = Counter
+        .builder("request")
+        .description("Total Request since application start on a Endpoint")
+        .tags("endpoint", "status", "method", "get")
+        .register(meterRegistry);
+  }
+
+
   @RequestMapping("/")
   public StatusDto getApplicationStatus(){
+    getCounter.increment();
     StatusDto status = new StatusDto();
     status.setStatus(true);
     status.setVersion(version);
