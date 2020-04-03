ce5b7849af5521d4698c70e8543dddf8daf84dba
Joost <MrKraboom@outlook.com>
2020-04-03 20:09:52 +0200

    Added VersionedBeanNameGenerator to Tests

--- src/main/java/de/sakpaas/backend/BackendApplication.java
+++ src/main/java/de/sakpaas/backend/BackendApplication.java
@@ -6,17 +6,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(nameGenerator = VersionedBeanNameGenerator.class)
 public class BackendApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BackendApplication.class);
     public static final String GENERATED = RandomString.make(64);
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(BackendApplication.class)
-                .beanNameGenerator(new VersionedBeanNameGenerator())
-                .run(args);
+        new SpringApplicationBuilder(BackendApplication.class).run(args);
 
         LOGGER.info("=========================================================================================");
         LOGGER.info("Database Generation Key: " + GENERATED);
