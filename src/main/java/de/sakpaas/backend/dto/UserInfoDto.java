14f7f6c9a9e0a9444a2dfc2b25f605255a5edae5
Robert Franzke <r.l.franzke@gmail.com>
2020-04-10 13:41:03 +0200

    Removed NoArgsConstructor in UserInfoDto

--- src/main/java/de/sakpaas/backend/dto/UserInfoDto.java
+++ src/main/java/de/sakpaas/backend/dto/UserInfoDto.java
@@ -3,13 +3,11 @@ package de.sakpaas.backend.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class UserInfoDto {
   private String id;
