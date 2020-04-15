0df4b5f96739d3865e92ed13accb95712d73e303
Robert Franzke <r.l.franzke@gmail.com>
2020-04-15 12:30:11 +0200

    Removed unused imports

--- src/main/java/de/sakpaas/backend/model/Address.java
+++ src/main/java/de/sakpaas/backend/model/Address.java
@@ -4,7 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
