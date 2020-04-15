88e9b2c2f525d9efbc8a17592ec200f04f2074b3
MrKraboom <MrKraboom@outlook.com>
2020-04-15 10:37:49 +0200

    Removed import @ComponentScan from test

--- src/test/java/de/sakpaas/backend/service/OccupancyServiceTest.java
+++ src/test/java/de/sakpaas/backend/service/OccupancyServiceTest.java
@@ -19,7 +19,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
