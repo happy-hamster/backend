223e5a8c25c14b87a0d94343d24b18806dda4d4c
Fabio Jungmann <fabio.jungmann@detect-value.com>
2020-04-18 19:27:05 +0200

    Unused imports entfernt

--- src/test/java/de/sakpaas/backend/service/ExceptionHandler.java
+++ src/test/java/de/sakpaas/backend/service/ExceptionHandler.java
@@ -1,8 +1,6 @@
 package de.sakpaas.backend.service;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import de.sakpaas.backend.HappyHamsterTest;
