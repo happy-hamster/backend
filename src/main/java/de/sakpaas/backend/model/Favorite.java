74859b7a2b5ed0b534fbb6c1d349748b0c82292c
benfu <benfu99@yahoo.de>
2020-04-19 20:06:32 +0200

    TINF-162: created Favorite Entity

---
+++ src/main/java/de/sakpaas/backend/model/Favorite.java
@@ -0,0 +1,29 @@
+package de.sakpaas.backend.model;
+
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity(name = "FAVORITE")
+public class Favorite {
+  @Id
+  @GeneratedValue
+  @Column(name = "ID", nullable = false)
+  private Long id;
+
+  //ManyToOne-Beziehung wird nicht dargestellt, da User in Keycloak gespeichert
+  @Column(name = "USER_UUID", nullable = false)
+  private UUID userUuid;
+
+  @JoinColumn(name = "LOCATION_ID", referencedColumnName = "ID", nullable = false)
+  @ManyToOne
+  private Location location;
+}
