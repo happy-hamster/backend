d8f01d23694d2b62802f9ff5ade49a6d2f06ab6f
David <davod2211@gmail.com>
2020-04-21 10:50:08 +0200

    test entfernt

--- src/test/java/de/sakpaas/backend/service/LocationApiSearchDasTest.java
+++ src/test/java/de/sakpaas/backend/service/LocationApiSearchDasTest.java
@@ -2,14 +2,10 @@ package de.sakpaas.backend.service;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import de.sakpaas.backend.HappyHamsterTest;
-import de.sakpaas.backend.dto.OsmResultLocationListDto;
 import de.sakpaas.backend.util.OsmImportConfiguration;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,18 +35,5 @@ class LocationApiSearchDasTest extends HappyHamsterTest {
             "way[shop=type1](area.searchArea);node[shop=type2](area.searchArea);" +
             "way[shop=type2](area.searchArea););out center;");
   }
-
-  @Test
-  void testEmptyShoptypeList() {
-    OsmImportConfiguration importConfiguration = new OsmImportConfiguration();
-
-
-    importConfiguration.setShoptypes(new ArrayList<>());
-    List<OsmResultLocationListDto.OsmResultLocationDto> results =
-        locationApiSearchDas.getLocationsForCountry(importConfiguration);
-
-    assertEquals(results, new ArrayList<>(), "Result List should be empty");
-
-  }
-
+  
 }
