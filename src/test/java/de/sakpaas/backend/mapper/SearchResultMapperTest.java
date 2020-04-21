bc73942220da056bb134a3f8d67db588c5237ad4
Marvin Deuschle <marvdeus@gmail.com>
2020-04-21 11:32:31 +0200

    corrected tests

--- src/test/java/de/sakpaas/backend/mapper/SearchResultMapperTest.java
+++ src/test/java/de/sakpaas/backend/mapper/SearchResultMapperTest.java
@@ -5,16 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import de.sakpaas.backend.HappyHamsterTest;
 import de.sakpaas.backend.model.Address;
+import de.sakpaas.backend.model.CoordinateDetails;
 import de.sakpaas.backend.model.Location;
 import de.sakpaas.backend.model.LocationDetails;
-import de.sakpaas.backend.service.SearchService.SearchResultObject;
+import de.sakpaas.backend.model.SearchResultObject;
 import de.sakpaas.backend.v2.dto.SearchResultDto;
 import de.sakpaas.backend.v2.mapper.SearchResultMapper;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,16 +29,15 @@ public class SearchResultMapperTest extends HappyHamsterTest {
 
   @Test
   public void testMapToDto() {
-    final Map<String, Double> coordinates = new HashMap<>();
-    coordinates.put("lat", 0.0);
-    coordinates.put("lon", 0.0);
+    final CoordinateDetails coordinates = new CoordinateDetails(0.0, 0.0);
     final LocationDetails locationDetails = new LocationDetails("type", "openingHours", "brand");
     final Address address = new Address();
     final List<Location> locations = new ArrayList<>(
         Collections.singletonList(new Location(1L, "test", 0.0, 0.0, locationDetails, address)));
     final SearchResultObject searchResultObject = new SearchResultObject(coordinates, locations);
 
-    final SearchResultDto mappedDto = searchResultMapper.mapLocationToOutputDto(searchResultObject);
+    final SearchResultDto mappedDto =
+        searchResultMapper.mapSearchResultToOutputDto(searchResultObject);
 
     // check for null
     assertNotNull(mappedDto);
