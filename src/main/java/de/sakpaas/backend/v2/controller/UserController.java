104f72a8ea7424a72d5b7ec6a17bbf004318bd50
benfu <benfu99@yahoo.de>
2020-04-21 13:36:25 +0200

    TINF-167: refactoration of logic in getFavorites() Method to Stream

--- src/main/java/de/sakpaas/backend/v2/controller/UserController.java
+++ src/main/java/de/sakpaas/backend/v2/controller/UserController.java
@@ -10,9 +10,9 @@ import de.sakpaas.backend.service.UserService;
 import de.sakpaas.backend.v2.dto.LocationResultLocationDto;
 import de.sakpaas.backend.v2.mapper.LocationMapper;
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -60,11 +60,12 @@ public class UserController {
       @RequestHeader("Authorization") String header, Principal principal) {
     UserInfoDto userInfo = userService.getUserInfo(header, principal);
     List<Favorite> favorites = favoriteRepository.findByUserUuid(UUID.fromString(userInfo.getId()));
-    List<LocationResultLocationDto> response = new ArrayList<>();
-    favorites.forEach(favorite -> {
-      Location location = favorite.getLocation();
-      response.add(locationMapper.mapToOutputDto(location));
-    });
+    List<LocationResultLocationDto> response = favorites.stream()
+        .map(favorite -> {
+          Location location = favorite.getLocation();
+          return locationMapper.mapToOutputDto(location);
+        })
+        .collect(Collectors.toList());
 
     return new ResponseEntity<>(response, OK);
   }
