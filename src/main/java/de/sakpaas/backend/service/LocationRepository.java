6547eb5fd84a783cd3cf40a83dc10d7547479776
Leaqua07 <lea.pellikan@gmail.com>
2020-04-23 15:29:58 +0200

    Repo angepasst - methode angepasst -fehlend: siehe storybeschreibung

--- src/main/java/de/sakpaas/backend/service/LocationRepository.java
+++ src/main/java/de/sakpaas/backend/service/LocationRepository.java
@@ -1,19 +1,23 @@
 package de.sakpaas.backend.service;
 
 import de.sakpaas.backend.model.Location;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LocationRepository extends JpaRepository<Location, Long> {
-  Optional<Location> findById(Long id);
+    Optional<Location> findById(Long id);
 
-  List<Location> findByLatitudeBetweenAndLongitudeBetween(Double latMin, Double latMax,
-                                                          Double lonMin, Double lonMax);
+    List<Location> findByLatitudeBetweenAndLongitudeBetween(Double latMin, Double latMax,
+                                                            Double lonMin, Double lonMax);
 
-  @Query(value = "SELECT loc.id FROM LOCATION loc JOIN ADDRESS a ON loc.address_id = a.id "
-      + "WHERE a.country=(:country)", nativeQuery = true)
-  List<Long> getAllIdsForCountry(@Param("country") String country);
+    List<Location> findByLatitudeBetweenAndLongitudeBetweenAndDetails_Type(Double latMin, Double latMax,
+                                                                           Double lonMin, Double lonMax, String Type);
+
+    @Query(value = "SELECT loc.id FROM LOCATION loc JOIN ADDRESS a ON loc.address_id = a.id "
+            + "WHERE a.country=(:country)", nativeQuery = true)
+    List<Long> getAllIdsForCountry(@Param("country") String country);
 }
