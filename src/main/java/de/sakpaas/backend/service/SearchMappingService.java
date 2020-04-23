8a4d76cde5ac3a94033023b4e91295c0b5fcbbb9
Robert Franzke <r.l.franzke@gmail.com>
2020-04-23 17:09:55 +0200

    Added Timer to Nominatim search Request

--- src/main/java/de/sakpaas/backend/service/SearchMappingService.java
+++ src/main/java/de/sakpaas/backend/service/SearchMappingService.java
@@ -4,13 +4,14 @@ import com.google.common.annotations.VisibleForTesting;
 import de.sakpaas.backend.dto.NominatimSearchResultListDto;
 import de.sakpaas.backend.exception.NoSearchResultsException;
 import de.sakpaas.backend.model.CoordinateDetails;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,12 +20,23 @@ public class SearchMappingService {
   private static final Logger LOGGER = LoggerFactory.getLogger(SearchMappingService.class);
 
   private final RestTemplate restTemplate;
+  private final MeterRegistry meterRegistry;
+
+  private Timer timer;
 
   @Value("${app.search-api-url}")
   private String searchApiUrl;
 
-  public SearchMappingService(RestTemplate restTemplate) {
+  public SearchMappingService(RestTemplate restTemplate,
+                              MeterRegistry meterRegistry) {
     this.restTemplate = restTemplate;
+    this.meterRegistry = meterRegistry;
+
+    Timer timer = Timer
+        .builder("nominatim.request")
+        .description("Times the duration of the Nominatim search requests")
+        .register(meterRegistry);
+
   }
 
   /**
@@ -56,11 +68,15 @@ public class SearchMappingService {
   @VisibleForTesting
   protected NominatimSearchResultListDto makeRequest(String url) {
     HttpEntity<String> header = setupRequest();
-    ResponseEntity<NominatimSearchResultListDto> response =
-        restTemplate
-            .exchange(url, HttpMethod.GET, header, NominatimSearchResultListDto.class);
 
-    return response.getBody();
+    try {
+      return timer.recordCallable(() -> restTemplate
+          .exchange(url, HttpMethod.GET, header, NominatimSearchResultListDto.class)).getBody();
+    } catch (Exception e) {
+      //sollte nicht vorkommen
+      e.printStackTrace();
+      return null;
+    }
   }
 
   /**
