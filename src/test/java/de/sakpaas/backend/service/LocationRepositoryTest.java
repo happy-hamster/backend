a012e27de0d6b07301814a45a53913d72e92dc65
Leaqua07 <lea.pellikan@gmail.com>
2020-04-28 15:18:45 +0200

    test hinzugefüt

--- src/test/java/de/sakpaas/backend/service/LocationRepositoryTest.java
+++ src/test/java/de/sakpaas/backend/service/LocationRepositoryTest.java
@@ -6,6 +6,8 @@ import de.sakpaas.backend.HappyHamsterTest;
 import de.sakpaas.backend.model.Address;
 import de.sakpaas.backend.model.Location;
 import de.sakpaas.backend.model.LocationDetails;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -55,4 +57,40 @@ class LocationRepositoryTest extends HappyHamsterTest {
 
   }
 
+  @Test
+  void findByLatitudeBetweenAndLongitudeBetweenAndDetails_Type() {
+    locationRepository.deleteAll();
+    locationDetailsRepository.deleteAll();
+
+    Address address =
+        new Address("testCountry", "testPostcode", "123456", "testStreet", "testNumber");
+    Address savedAddress = addressRepository.save(address);
+
+    LocationDetails locationDetails =
+        new LocationDetails("testType", "testOpeningHours", "testBrand");
+    LocationDetails savedLocationDetails = locationDetailsRepository.save(locationDetails);
+
+    Location location = new Location(1L, "LIDL", 41.0D, 8.0D, savedLocationDetails, savedAddress);
+    locationRepository.save(location);
+
+
+    Address address1 =
+        new Address("testCountry", "testPostcode", "123456", "testStreet", "testNumber");
+    Address savedAddress1 = addressRepository.save(address1);
+
+    LocationDetails locationDetails1 =
+        new LocationDetails("testType1", "testOpeningHours", "testBrand");
+    LocationDetails savedLocationDetails1 = locationDetailsRepository.save(locationDetails1);
+
+    Location location1 = new Location(2L, "LIDL", 40.0D, 7.0D, savedLocationDetails, savedAddress);
+    locationRepository.save(location1);
+
+    List<Location>
+        list = locationRepository
+        .findByLatitudeBetweenAndLongitudeBetweenAndDetails_Type(30.D, 42.D, 6.D, 9.0,
+            Arrays.asList("testtype1"));
+
+    assertThat(list.size()).isEqualTo(1);
+    assertThat(list.get(0)).isEqualTo(location1);
+  }
 }
