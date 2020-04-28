113424534e4951085233b546d4c6302897896b4a
MrKraboom <MrKraboom@outlook.com>
2020-04-28 16:41:53 +0200

    Removed merge annotations

--- src/main/java/de/sakpaas/backend/v2/controller/LocationController.java
+++ src/main/java/de/sakpaas/backend/v2/controller/LocationController.java
@@ -37,10 +37,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<<HEAD
-=======
-    >>>>>>>develop
-
 @CrossOrigin(origins = "*")
 @RequestMapping("/v2/locations")
 @RestController
