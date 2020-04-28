24a3216b42856a067fd401b09ce18131daa5faa5
Robert Franzke <r.l.franzke@gmail.com>
2020-04-28 21:55:34 +0200

    Created Repo Query and Test for it

--- src/main/java/de/sakpaas/backend/service/LocationDetailsRepository.java
+++ src/main/java/de/sakpaas/backend/service/LocationDetailsRepository.java
@@ -2,10 +2,15 @@ package de.sakpaas.backend.service;
 
 import de.sakpaas.backend.model.LocationDetails;
 import java.util.Optional;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LocationDetailsRepository extends JpaRepository<LocationDetails, Long> {
 
   Optional<LocationDetails> findById(Long id);
 
+  @Query(value = "select lower(brand) from location_details where brand is not null group by brand",
+      nativeQuery = true)
+  Set<String> getAllBrandNamesLower();
 }
