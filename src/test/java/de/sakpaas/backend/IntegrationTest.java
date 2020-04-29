139ce111979348257765340a6a0d995f2a60f618
MrKraboom <MrKraboom@outlook.com>
2020-04-29 18:08:43 +0200

    Cleaned up Integration tests

--- src/test/java/de/sakpaas/backend/IntegrationTest.java
+++ src/test/java/de/sakpaas/backend/IntegrationTest.java
@@ -1,5 +1,49 @@
 package de.sakpaas.backend;
 
+import de.sakpaas.backend.service.UserService;
+import java.util.UUID;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.keycloak.common.VerificationException;
+import org.keycloak.representations.AccessToken;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.web.servlet.MockMvc;
+
 public class IntegrationTest extends HappyHamsterTest {
 
+  @Autowired
+  protected MockMvc mockMvc;
+
+  @SpyBean
+  protected UserService userService;
+
+  public static final UUID USER_UUID = UUID.fromString("550e8400-e29b-11d4-a716-446655440000");
+  public static final AccessToken USER_ACCESS_TOKEN = new AccessToken();
+  public static final String AUTHENTICATION_VALID = "Bearer token.valid.token";
+  public static final String AUTHENTICATION_INVALID = "Bearer token.invalid.token";
+
+  @BeforeAll
+  static void setupAll() {
+    USER_ACCESS_TOKEN.setSubject(USER_UUID.toString());
+    USER_ACCESS_TOKEN.setPreferredUsername("test.user");
+    USER_ACCESS_TOKEN.setName("Testonius Tester");
+    USER_ACCESS_TOKEN.setGivenName("Testonius");
+    USER_ACCESS_TOKEN.setFamilyName("Tester");
+    USER_ACCESS_TOKEN.setEmail("testonius.tester@example.com");
+  }
+
+  @SneakyThrows
+  @BeforeEach
+  void setup() {
+    Mockito.doAnswer(invocation -> {
+      if (invocation.getArgument(0).equals("token.valid.token"))
+        return USER_ACCESS_TOKEN;
+      throw new VerificationException();
+    })
+        .when(userService)
+        .verifyToken(Mockito.any());
+  }
 }
--- src/test/java/de/sakpaas/backend/v2/controller/LocationControllerTest.java
+++ src/test/java/de/sakpaas/backend/v2/controller/LocationControllerTest.java
@@ -4,54 +4,19 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import de.sakpaas.backend.IntegrationTest;
-import de.sakpaas.backend.service.UserService;
-import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.keycloak.common.VerificationException;
-import org.keycloak.representations.AccessToken;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 class LocationControllerTest extends IntegrationTest {
 
-  @Autowired
-  private MockMvc mockMvc;
-
-  @SpyBean
-  protected UserService userService;
-
-  public static final UUID USER_UUID = UUID.fromString("550e8400-e29b-11d4-a716-446655440000");
-  public static final String AUTHENTICATION_VALID = "Bearer token.valid.token";
-  public static final String AUTHENTICATION_INVALID = "Bearer token.invalid.token";
-
   @Test
   void getById() throws Exception {
-    AccessToken accessToken = new AccessToken();
-    accessToken.setSubject(USER_UUID.toString());
-    accessToken.setPreferredUsername("test.user");
-    accessToken.setName("Testonius Tester");
-    accessToken.setGivenName("Testonius");
-    accessToken.setFamilyName("Tester");
-    accessToken.setEmail("testonius.tester@example.com");
-
-    Mockito.doAnswer(invocation -> {
-      System.out.println((Object) invocation.getArgument(0));
-      if (invocation.getArgument(0).equals("token.valid.token"))
-        return accessToken;
-      throw new VerificationException();
-    })
-        .when(userService)
-        .verifyToken(Mockito.any());
-
     mockMvc.perform(
         get("/v2/locations/1000")
             .header("Authorization", AUTHENTICATION_INVALID))
@@ -60,6 +25,10 @@ class LocationControllerTest extends IntegrationTest {
     mockMvc.perform(
         get("/v2/locations/1000")
             .header("Authorization", AUTHENTICATION_VALID))
+        .andExpect(status().isNotFound());
+
+    mockMvc.perform(
+        get("/v2/locations/1000"))
         .andExpect(status().isNotFound());
   }
 }
